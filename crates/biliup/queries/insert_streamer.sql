332f0e5a48aad3b8866a64eca5c6f167dcb97ec3
Kataick <172830791@qq.com>
2023-06-06 17:05:16 +0800

    修复(#80)的问题

--- crates/biliup/queries/insert_streamer.sql
+++ crates/biliup/queries/insert_streamer.sql
@@ -39,7 +39,7 @@ returning
     dolby as "dolby!: u8",
     lossless_music as "lossless_music!: u8",
     no_reprint as "no_reprint: u8",
+    open_elec as "open_elec: u8"
     up_selection_reply as "up_selection_reply!: bool",
     up_close_reply as "up_close_reply!: bool",
     up_close_danmu as "up_close_danmu!: bool",
-    open_elec as "open_elec: u8"
