4fc7b2d8ae043a830585708f285846c701df056d
Kataick <kkm@katck.com>
2024-06-03 23:08:56 +0800

    try to fix

--- crates/stream-gears/src/lib.rs
+++ crates/stream-gears/src/lib.rs
@@ -192,7 +192,7 @@ fn login_by_web_qrcode(sess_data: String, dede_user_id: String) -> PyResult<bool
 
 #[allow(clippy::too_many_arguments)]
 #[pyfunction]
-#[pyo3(signature = (video_path, cookie_file, title, tid=171, tag=None, copyright=2, source=None, desc=None, dynamic=None, cover=None, dolby=0, lossless_music=0, no_reprint=0, open_elec=0, up_close_reply=false, limit=3, desc_v2=vec![], dtime=None, line=None))]
+#[pyo3(signature = (video_path, cookie_file, title, tid=171, tag="".to_string(), copyright=2, source="".to_string(), desc="".to_string(), dynamic="".to_string(), cover="".to_string(), dolby=0, lossless_music=0, no_reprint=0, open_elec=0, up_close_reply=false, limit=3, desc_v2=vec![], dtime=None, line=None))]
 fn upload(
     py: Python<'_>,
     video_path: Vec<PathBuf>,
