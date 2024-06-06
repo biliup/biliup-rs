2a29729e073b7f581ce7c4f9e47cff773e4b7c35
ForgQi <34411314+ForgQi@users.noreply.github.com>
2024-06-06 23:23:14 +0800

    Bumping version

--- crates/stream-gears/src/lib.rs
+++ crates/stream-gears/src/lib.rs
@@ -192,7 +192,6 @@ fn login_by_web_qrcode(sess_data: String, dede_user_id: String) -> PyResult<bool
 
 #[allow(clippy::too_many_arguments)]
 #[pyfunction]
-//#[pyo3(signature = (video_path, cookie_file, title, tid=171, tag="".to_string(), copyright=2, source="".to_string(), desc="".to_string(), dynamic="".to_string(), cover="".to_string(), dolby=0, lossless_music=0, no_reprint=0, open_elec=0, up_close_reply=false, up_selection_reply=false, limit=3, desc_v2=vec![], dtime=None, line=None))]
 fn upload(
     py: Python<'_>,
     video_path: Vec<PathBuf>,
@@ -276,6 +275,7 @@ fn upload(
 
 #[allow(clippy::too_many_arguments)]
 #[pyfunction]
+#[pyo3(signature = (video_path, cookie_file, title, tid=171, tag="".to_string(), copyright=2, source="".to_string(), desc="".to_string(), dynamic="".to_string(), cover="".to_string(), dolby=0, lossless_music=0, no_reprint=0, open_elec=0, up_close_reply=false, up_selection_reply=false, up_close_danmu=false, limit=3, desc_v2=vec![], dtime=None, line=None))]
 fn upload_by_app(
     py: Python<'_>,
     video_path: Vec<PathBuf>,
